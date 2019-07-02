e858e2269c1431f879f28b60296d2f92fad80e57
Boris Kobzarev <planez@bk.ru>
2019-07-02 23:49:28 +0300

    Запись данных в таблицу

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
+const path = require('path')
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
 const pg = require('pg')
-const conString = 'postgres://postgres:123456@localhost:5432/node_hero'
+const conString = 'postgres://postgres:123456@localhost/node_hero'
 var pool = new pg.Pool({connectionString: conString})
 
-pool.connect(function (err, client, done) {
-  if (err) {
-    return console.error('error fetching client from pool', err)
-  }
-  client.query('SELECT $1::varchar AS my_first_query', ['node hero'], function (err, result) {
-    done()
 
+
+
+  pool.connect(function (err, client, done) {
     if (err) {
-      return console.error('error happened during query', err)
+      // Передача ошибки в обработчик express
+      return next(err)
     }
-    console.log(result.rows[0])
-    pool.end()
+    client.query('INSERT INTO users (name, age) VALUES (\'Oleg\', 32);', function (err, result) {
+      pool.end() // Этот коллбек сигнализирует драйверу pg, что соединение может быть закрыто или возвращено в пул соединений
+      if (err) {
+        // Передача ошибки в обработчик express
+        return next(err)
+      }
+      res.send(200)
+    })
   })
-})
