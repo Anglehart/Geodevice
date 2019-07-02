31be0b0e8d9483e4358d9ba4088232ba033d4232
Boris Kobzarev <planez@bk.ru>
2019-07-03 01:10:29 +0300

    Нужна помощь, смотри index.js

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
+const path = require('path')
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
 const pg = require('pg')
 const conString = 'postgres://postgres:123456@localhost/node_hero'
 var pool = new pg.Pool({connectionString: conString})
+
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+app.listen(3000)
+app.get('/', (request, response) => {
+    response.render('home', {
+        name: 'Юрий',
+        age: '28'
+    })
+})
+
+app.post('/', function (req, res, next) {
+  const user = req.body
 
   pool.connect(function (err, client, done) {
     if (err) {
       // Передача ошибки в обработчик express
       return next(err)
     }
-    client.query('INSERT INTO users (name, age) VALUES (\'Oleg\', 32);', function (err, result) {
+    client.query('INSERT INTO users (name, age) VALUES ($1, $2);', [user.name, user.age], function (err, result) {
       pool.end() // Этот коллбек сигнализирует драйверу pg, что соединение может быть закрыто или возвращено в пул соединений
       if (err) {
         // Передача ошибки в обработчик express
@@ -15,3 +37,4 @@ var pool = new pg.Pool({connectionString: conString})
       }
     })
   })
+})
