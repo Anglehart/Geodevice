b22fd8070c4bdbae3039494441cb38fca7d7ef86
Boris Kobzarev <planez@bk.ru>
2019-07-07 23:34:51 +0300

    Нерабочий get

--- index.js
+++ index.js
@@ -17,10 +17,23 @@ app.engine('.hbs', exphbs({
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'views'))
 app.listen(3000)
-app.get('/userage', (request, response) => {
-    response.render('home')
+
+//Этот запрос должен получать из базы все данные и рендерить их. Но не работает =(
+app.get('/userage', (request, response, next) => {
+  pool.connect(function (err, client, done) {
+    if (err) {
+      return next(err)
+    }
+    client.query('SELECT name, age FROM users;', [], function (err, result) {
+      if (err) {
+        return next(err)
+      }
+      response.render('home', {object: result});
+    })
+  })
 })
 
+//Этот запрос работает и отправляет данные в базу, а потом заново рендерит страницу
 app.post("/userage", urlencodedParser, function (request, response, next) {
     if(!request.body) return response.sendStatus(400);
     pool.connect(function (err, client, done) {
@@ -28,10 +41,11 @@ app.post("/userage", urlencodedParser, function (request, response, next) {
         return next(err)
       }
       client.query('INSERT INTO users (name, age) VALUES ($1, $2);', [request.body.userName, request.body.userAge], function (err, result) {
+        //pool.end() - если закрыть пул, то при повторном обращении выдает ошибку
         if (err) {
           return next(err)
         }
-      response.render('home')
+      response.render('home') //После выполнения post-запроса браузер ожидает ответа, поэтому отправляем ему рендер страницы
       })
     })
 });
