ca571cb783ddf76ccefafaedc08727f5fc65f76f
Boris Kobzarev <planez@bk.ru>
2019-07-07 22:03:40 +0300

    Добавление в базу из формы

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const app = express()
 const pg = require('pg')
+const bodyParser = require("body-parser");
 const conString = 'postgres://postgres:123456@localhost/node_hero'
 var pool = new pg.Pool({connectionString: conString})
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+
 
 app.engine('.hbs', exphbs({
     defaultLayout: 'main',
@@ -14,29 +17,40 @@ app.engine('.hbs', exphbs({
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'views'))
 app.listen(3000)
-app.get('/', (request, response) => {
-    response.render('home', {
-        name: 'Юрий',
-        age: '28'
-    })
+app.get('/userage', (request, response) => {
+    response.render('home')
 })
 
-
-app.post('/', function (req, res, next) {
-  const user = req.body
-  //Что мы пытаемся присвоить константе user?
-  //По идее, в нее надо положить значения name и age, которые отрендерились на странице (или перехватить до этого)
-  //Если убрать обертку app.post и заявить переменные явно, то они запишутся в базу (я закоммитил это)
-  //В таком виде код просто подвешивает консоль
-  pool.connect(function (err, client, done) {
-    if (err) {
-      return next(err)
-    }
-    client.query('INSERT INTO users (name, age) VALUES ($1, $2);', [user.name, user.age], function (err, result) {
-      pool.end()
+app.post("/userage", urlencodedParser, function (request, response) {
+    if(!request.body) return response.sendStatus(400);
+    pool.connect(function (err, client, done) {
       if (err) {
         return next(err)
       }
+      client.query('INSERT INTO users (name, age) VALUES ($1, $2);', [request.body.userName, request.body.userAge], function (err, result) {
+        pool.end()
+        if (err) {
+          return next(err)
+        }
+      })
     })
-  })
-})
+});
+
+/*app.post("/userage", urlencodedParser, function (request, response, next) {
+    if(!request.body) return response.sendStatus(400);
+    pool.connect(function (err, client, done) {
+      if (err) {
+        return next(err)
+      }
+      client.query('INSERT INTO users (name, age) VALUES ($1, $2);', [request.body.userName, request.body.userAge], function (err, result) {
+        pool.end()
+        if (err) {
+          return next(err)
+        }
+      })
+    })
+});*/
+
+app.get("/", function(request, response){
+    response.send("Главная страница");
+});
