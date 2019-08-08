5718b7d0e4a965451f7f16fd5e8c1e12e9db3e36
Boris Kobzarev <planez@bk.ru>
2019-08-08 23:01:06 +0300

    Изменил структуру проекта

---
+++ geodevice/server/index.js
@@ -0,0 +1,10 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const usersRouter = require('./userRouter');
+
+app.use(express.json());
+app.use('/user', usersRouter);
+app.use('/', express.static (path.join(__dirname, '../client')))
+
+app.listen(3000)
---
+++ geodevice/server/userRouter.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+const db = require('./connection'); //для модуля из этой же папки обязательно ставить ./
+
+router.post("/", function (request, response) { //Добавление пользователя, возврат ID
+  if(!request.body) return response.sendStatus(400);
+  db.one('INSERT INTO users (name, age) VALUES ($1, $2) RETURNING id', [request.body.userName, request.body.userAge])
+    .then(function (data) {
+      return response.json(data);
+    })
+    .catch(function (error) {
+      console.log("ERROR:", error);
+  });
+});
+
+router.get("/", function (request, response) { //Показать всех пользователей
+  if(!request.body) return response.sendStatus(400);
+  db.any('SELECT name, age, id FROM users;')
+  .then(function (data) {
+    return response.json(data);
+  })
+})
+
+router.delete("/id", function (request, response) { //Удалить пользователя по ID
+  db.any('DELETE FROM users WHERE id = $1 RETURNING id;', [request.query.userId])
+    .then(function (data) {
+      return response.json(data);
+    })
+    .catch(function (error) {
+      console.log("ERROR:", error);
+  });
+});
+
+router.get("/id", function (request, response) { //Возвращает одного пользователя
+  db.one('SELECT name, age, id FROM users WHERE id = $1;', [request.query.userId])
+  .then(function (data) {
+    return response.json(data);
+  })
+})
+
+router.put("/id", function (request, response) { //Изменить пользователя по ID
+  if(!request.body) return response.sendStatus(400);
+  db.one('UPDATE Users SET name = $1, age = $2 WHERE id = $3 RETURNING id, name, age; ', [request.body.userName, request.body.userAge, request.body.userId])
+    .then(function (data) {
+      console.log(data);
+      return response.json(data);
+    })
+    .catch(function (error) {
+      return response.json(error);
+  });
+});
+module.exports = router;
