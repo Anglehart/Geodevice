b11f0dd5b453f2cbf4361832d56bfb80f8a95431
Boris Kobzarev <planez@bk.ru>
2019-08-08 23:30:56 +0300

    Форма регистрации, не работает

--- geodevice/src/App.js
+++ geodevice/src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class RegisterForm extends React.Component {
+
+    render() {
+      return (
+        <form name="registerForm">
+            <label>Имя</label><br>
+            <input type="text" name="userName" /><br><br>
+            <label>Возраст</label></br>
+            <input type="number" name="userAge" /><br><br>
+            <label>ID</label></br>
+            <input type="number" name="userId" /><br><br>
+            <button type="submit" id="newUser">Добавить пользователя</button><br>
+            <button type="submit" id="showAll">Показать всех пользователей</button><br>
+            <button type="submit" id="deleteUser">Удалить пользователя по ID</button><br>
+            <button type="submit" id="showUser">Показать пользователя по ID</button><br>
+            <button type="submit" id="editUser">Изменить пользователя по ID</button>
+        </form>
+    )
+  }
 }
 
-export default App;
+export default RegisterForm;
